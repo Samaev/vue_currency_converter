61aec8e01d86434cbb43f17fcb69f46f72610ba1
Andrey Samaev <andreysamaev@gmail.com>
2023-02-25 14:13:21 +0200

    module replaced

---
+++ src/components/Converter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="converter">
+    <h2>Currency Exchange</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConvertArea',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
